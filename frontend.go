d9822d2e14e3838467b102d248779e842349f40c
palidanx <palidanx@pali-xps-13.localdomain>
2020-05-27 05:51:10 -0700

    removing google analytics

--- frontend.go
+++ frontend.go
@@ -11,15 +11,6 @@ const tpl = `
 <!DOCTYPE html>
 <html>
     <head>
-		<!-- Global site tag (gtag.js) - Google Analytics -->
-		<script async src="https://www.googletagmanager.com/gtag/js?id=UA-166905893-1"></script>
-		<script>
-		  window.dataLayer = window.dataLayer || [];
-		  function gtag(){dataLayer.push(arguments);}
-		  gtag('js', new Date());
-
-		  gtag('config', 'UA-166905893-1');
-		</script>
         <title>Codenames Pictures - Play Online</title>
         <script src="/js/lib/browser.min.js"></script>
         <script src="/js/lib/react.min.js"></script>
